57cab6d396a1f3972636499959f209bf198ed510
Hakim <elbakk_a@Mac-mini-de-Morvan.local>
2015-07-28 11:33:52 +0200

    Commiting the Moskito project

---
+++ Moskito/Moskito/Moskito.h
@@ -0,0 +1,14 @@
+//
+//  Moskito.h
+//  Moskito
+//
+//  Created by Hakim on 27/07/15.
+//  Copyright (c) 2015 Hakim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import  "libZeta.h"
+
+@interface Moskito : NSObject
+
+@end
---
+++ Moskito/Moskito/Moskito.m
@@ -0,0 +1,13 @@
+//
+//  Moskito.m
+//  Moskito
+//
+//  Created by Hakim on 27/07/15.
+//  Copyright (c) 2015 Hakim. All rights reserved.
+//
+
+#import "Moskito.h"
+
+@implementation Moskito
+
+@end
